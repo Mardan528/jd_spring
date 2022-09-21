996a2ff82a4fb4f058b190a4b721db4c37889c6e
“xiayimard <karwanghana@gmail.com>
2022-09-21 16:19:13 +0100

    config

--- Spring-01-SpringContainerr/src/main/java/CybertakApp.java
+++ Spring-01-SpringContainerr/src/main/java/CybertakApp.java
@@ -1,4 +1,6 @@
+import interfaces.Mentor;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertakApp {
@@ -6,8 +8,18 @@ public class CybertakApp {
     public static void main(String[] args) {
 
 
-        BeanFactory container=new ClassPathXmlApplicationContext("config.xml");
+        //BeanFactory container=new ClassPathXmlApplicationContext("config.xml");
 
+        // do same thing
+        ApplicationContext container=new ClassPathXmlApplicationContext("config.xml");
+
+        Mentor mentor= (Mentor) container.getBean("fullTimeMentor");
+        // this way no need to down casting
+        Mentor mentor2=container.getBean("partTimeMentor",Mentor.class);
+       // Mentor mentor2= (Mentor) container.getBean("partTimeMentor");
+
+        mentor.createAccount();
+        mentor2.createAccount();
 
 
     }
