32af1ca56f61d126ef61f0a098c91aea1cee21f3
“xiayimard <karwanghana@gmail.com>
2022-09-29 14:12:04 +0100

    Spring 02 singleton and prototype

---
+++ Spring-03-BeansScope-Singleton-Prototype/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,14 @@
+package com.cybertek.services;
+
+import com.cybertek.Interfacec.Course;
+
+public class Java implements Course {
+
+
+
+    @Override
+    public void getTeachHours() {
+
+        System.out.println(" Weekly teaching hours : 20" );
+    }
+}
