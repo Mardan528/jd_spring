2293406ae4fa7458f0e6d331d04b56a898232699
“xiayimard <karwanghana@gmail.com>
2022-09-29 10:42:59 +0100

    Spring 02 construction

--- Spring-02-Di-ConstreuctionInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-Di-ConstreuctionInjection/src/main/java/com/cybertek/services/Java.java
@@ -3,8 +3,17 @@ package com.cybertek.services;
 import com.cybertek.Interfacec.Course;
 
 public class Java implements Course {
+
+    OfficeHourse officeHourse;
+
+    public Java(OfficeHourse officeHourse) {
+
+        this.officeHourse = officeHourse;
+    }
+
     @Override
     public void getTeachHours() {
-        System.out.println(" Weekly teaching hours :20");
+
+        System.out.println(" Weekly teaching hours :" +(20+ officeHourse.getHours()));
     }
 }
---
+++ Spring-02-Di-ConstreuctionInjection/src/main/java/com/cybertek/services/OfficeHourse.java
@@ -0,0 +1,10 @@
+package com.cybertek.services;
+
+import com.cybertek.Interfacec.ExtraSessions;
+
+public class OfficeHourse implements ExtraSessions {
+    @Override
+    public int getHours() {
+        return 5;
+    }
+}
